8383278649dc3e1c379caef49a5ecc8112990764
caroidarraga <caroidarraga@gmail.com>
2022-10-08 18:45:13 -0500

    Initial commit

---
+++ src/main/java/com/veterinarias/firulapp/FirulappApplication.java
@@ -0,0 +1,13 @@
+package com.veterinarias.firulapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirulappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirulappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veterinarias/firulapp/FirulappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinarias.firulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirulappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
